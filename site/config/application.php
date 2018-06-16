351c438f0f5d76d59edcae53de82199ad4eedbc1
edelstejn <i.grinstejn@gmail.com>
2018-06-16 21:36:15 +0200

    going-live

--- site/config/application.php
+++ site/config/application.php
@@ -74,9 +74,14 @@ define('NONCE_SALT', env('NONCE_SALT'));
 define('AUTOMATIC_UPDATER_DISABLED', true);
 define('DISABLE_WP_CRON', env('DISABLE_WP_CRON') ?: false);
 define('DISALLOW_FILE_EDIT', true);
-/**/
-define( 'WP_MEMORY_LIMIT', '120M' );
-/**/
+
+define( 'WP_MEMORY_LIMIT', '96M' );
+ini_set('display_errors','Off');
+ini_set('error_reporting', E_ALL );
+define('WP_DEBUG', false);
+define('WP_DEBUG_DISPLAY', false);
+
+
 /**
  * Bootstrap WordPress
  */
