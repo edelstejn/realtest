6f9bc5bcb7ffdf23ccfbe3ecc800f90a8d23a1f1
edelstejn <i.grinstejn@gmail.com>
2018-06-17 14:16:33 +0200

    nextt

--- site/config/application.php
+++ site/config/application.php
@@ -79,7 +79,7 @@ define( 'WP_MEMORY_LIMIT', '96M' );
 ini_set('display_errors','Off');
 ini_set('error_reporting', E_ALL );
 define('WP_DEBUG', false);
-/*define('WP_DEBUG_DISPLAY', false);*/
+define('WP_DEBUG_DISPLAY', false);
 
 
 /**
