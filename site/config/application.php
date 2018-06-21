2d1132a5612d18a8e4973eb688b78d32c212c773
edelstejn <i.grinstejn@gmail.com>
2018-06-21 20:14:56 +0200

    losjetzt!

--- site/config/application.php
+++ site/config/application.php
@@ -78,9 +78,10 @@ define('DISALLOW_FILE_EDIT', true);
 define( 'WP_MEMORY_LIMIT', '96M' );
 ini_set('display_errors','Off');
 ini_set('error_reporting', E_ALL );
+/*
 define('WP_DEBUG', false);
 define('WP_DEBUG_DISPLAY', false);
-
+*/
 
 /**
  * Bootstrap WordPress
