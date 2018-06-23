f1c5fe625753db06225b784cedad694107a59f1e
edelstejn <i.grinstejn@gmail.com>
2018-06-23 19:58:14 +0200

    new-approach-finding-error, all debugs swithed on - where are you bitchy error???!

--- site/config/application.php
+++ site/config/application.php
@@ -78,11 +78,23 @@ define('DISALLOW_FILE_EDIT', true);
 define( 'WP_MEMORY_LIMIT', '96M' );
 ini_set('display_errors','Off');
 ini_set('error_reporting', E_ALL );
-/*
-define('WP_DEBUG', false);
-define('WP_DEBUG_DISPLAY', false);
-*/
 
+/*define('WP_DEBUG', false);*/
+define('WP_DEBUG_DISPLAY', false);
+
+// Enable WP_DEBUG mode
+define( 'WP_DEBUG', true );
+
+// Enable Debug logging to the /wp-content/debug.log file
+define( 'WP_DEBUG_LOG', true );
+
+// Disable display of errors and warnings 
+define( 'WP_DEBUG_DISPLAY', false );
+@ini_set( 'display_errors', 0 );
+
+// Use dev versions of core JS and CSS files (only needed if you are modifying these core files)
+define( 'SCRIPT_DEBUG', true );
+ 
 /**
  * Bootstrap WordPress
  */
