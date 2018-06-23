63f6decb8037cad476ccfb5bb83eefdd35b6a96e
edelstejn <i.grinstejn@gmail.com>
2018-06-23 20:17:01 +0200

    debug error fix

--- site/config/application.php
+++ site/config/application.php
@@ -80,7 +80,7 @@ ini_set('display_errors','Off');
 ini_set('error_reporting', E_ALL );
 
 /*define('WP_DEBUG', false);*/
-define('WP_DEBUG_DISPLAY', false);
+/*define('WP_DEBUG_DISPLAY', false);*/
 
 // Enable WP_DEBUG mode
 define( 'WP_DEBUG', true );
